e576f80d15b937f0d3f0ab4c1669d8e1bd2ed41a
jeremybaby <JeremyLiu.will@gmail.com>
2019-10-12 15:29:00 +0800

    git baseball java

---
+++ Java/src/_682/baseball/game/Solution.java
@@ -0,0 +1,31 @@
+package _682.baseball.game;
+
+import java.util.Stack;
+
+public class Solution {
+    public int calPoints(String[] ops) {
+    	
+    	int ans = 0;
+    	Stack<Integer> stack = new Stack();
+    	
+    	for (String op: ops) {
+    		if (op.equals("+")) {
+    			int top = stack.pop();
+    			int newtop = top + stack.peek();
+    			stack.push(top);
+    			stack.push(newtop);
+    		} else if (op.equals("C")) {
+    			stack.pop();
+    		} else if (op.equals("D")) {
+    			stack.push(2 * stack.peek());
+    		} else {
+    			stack.push(Integer.valueOf(op));
+    		}
+    	}
+    	
+    	for (int score: stack)
+    		ans += score;
+    	
+    	return ans;
+    }
+}
