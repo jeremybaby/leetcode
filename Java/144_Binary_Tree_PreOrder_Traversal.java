92fe3139c99d6a2cb66e7b5261aa8ca9c1261680
jeremybaby <JeremyLiu.will@gmail.com>
2019-11-17 14:55:40 +0800

    144 Java PreOrder

---
+++ Java/144_Binary_Tree_PreOrder_Traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/* 递归 */
+class Solution1 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        helper(res, root);
+        return res;
+    }
+
+    public static void helper(List<Integer> res, TreeNode root) {
+        if (root == null) return;
+        res.add(root.val);
+        helper(res, root.left);
+        helper(res, root.right);
+    }
+}
+
+/**
+ *  迭代：用栈实现
+ *  注意：递归的本质其实就是系统栈 
+ */
+class Solution2 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur.right != null) stack.push(cur.right);
+            if (cur.left != null) stack.push(cur.left);
+            res.add(cur.val);
+        }
+        return res;
+    }
+}
